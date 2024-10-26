e52051ecaf6b6c3bc80d1679bbc8d412096bff03
jlakhan <jlakhan@ncsu.edu>
2024-10-26 19:32:10 -0400

    Added unit conversions for Frequency, Angles, and Energy

--- code/main_scripts/popular_units.js
+++ code/main_scripts/popular_units.js
@@ -7,46 +7,67 @@
  * @type {Array}
  */
 const POPULAR_UNITS = {
-  length: [
-    "miles",
-    "meters",
-    "kilometers",
-    "yard",
-    "feet",
-    "furlong",
-    "chain",
-    "centimeters",
-    "millimeters",
-    "inch"
-  ],
-  mass: [
-    "kilograms",
-    "pounds",
-    "ounces",
-    "grams",
-    "tonnes",
-    "milligrams",
+  length: ["miles", "meters", "kilometers", "yard", "feet", "furlong", "chain", "centimeters", "millimeters", "inch", "nautical mile", "micrometer", "nanometer"], 
+ mass: [
+    "kilograms", 
+    "pounds", 
+    "ounces", 
+    "grams", 
+    "tonnes", 
+    "milligrams", 
     "micrograms",
-  ],
-  temperature: ["celcius", "fahrenheit", "kelvin"],
+    "imperial ton", 
+    "us ton",       
+    "stone"         
+  ],  temperature: ["celcius", "fahrenheit", "kelvin"],
   currency: ["usd", "eur", "gbp", "inr", "jpy", "cad", "aud", "chf", "cny"],
-  time: ["seconds", "minutes", "hours", "days", "weeks"],
-  dataTransfer: ["Mb/s", "Gb/s", "Tb/s","b/s", "Kb/s"],
-  area: ["km2", "miles2", "ft2","ha2", "acre2", "yd2","m2","in2","cm2"],
-  speed: ["Km/h", "mph","m/s"],
-  volume: [
-    "litres",
-    "milliliter",
-    "cubic meter",
-    "cubic inch",
-    "cubic foot",
-    "pint",
-    "quart",
-    "gallon",
-    "fl oz",
+  time: ["seconds", "minutes", "hours", "days", "weeks", "milliseconds", "microseconds", "nanoseconds"], 
+  dataTransfer: [
+    "b/s",        
+    "Kb/s",         
+    "Kib/s",       
+    "Mb/s",        
+    "Mib/s",      
+    "Gb/s",       
+    "Gib/s",      
+    "Tb/s",        
+    "Tib/s",      
+    "KB/s",      
+    "MB/s",      
+    "GB/s",       
+    "TB/s"         
+  ],  area: ["km2", "miles2", "ft2", "ha2", "acre2", "yd2", "m2", "in2", "cm2"],
+  speed: ["Km/h", "mph", "m/s"],
+  volume: ["litres", "milliliter", "cubic meter", "cubic inch", "cubic foot", "pint", "quart", "gallon", "fl oz", "cup", "tablespoon", "teaspoon"], 
+  data: ["MB", "GB", "TB", "B", "KB"],
+  energy: [
+    "joule",
+    "kilojoule",
+    "gram calorie",
+    "kilocalorie",
+    "watt hour",
+    "kilowatt hour",
+    "electronvolt",
+    "british thermal unit",
+    "us therm",
+    "foot-pound"
   ],
-  data: ["MB", "GB", "TB","B", "KB"],
+  frequency: [
+    "hertz",
+    "kilohertz",
+    "megahertz",
+    "gigahertz"
+  ],
+  angle: [
+    "degree",
+    "arcsecond",
+    "gradian",
+    "milliradian",
+    "minute of arc",
+    "radian"
+  ]
 };
+
 
 const PRE_SYMBOLS = new Set([
   "$",
