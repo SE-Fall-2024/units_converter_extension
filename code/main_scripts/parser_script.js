05c2e878973ab535aa820d691161bd542c00e747
hannahestes <hannahestes6@gmail.com>
2024-11-22 10:32:39 -0500

    Fixed functionality for aligning popup and modal

--- code/main_scripts/parser_script.js
+++ code/main_scripts/parser_script.js
@@ -10,7 +10,6 @@ async function get_conversions(selection) {
     /((^[\-−]?(?:\d+|\d{1,3}(?:,\d{3})+)(?:(\.|,)\d+)?))\s*/
   );
   let result = '';
-
   if (/\b\d{1,2}:\d{2}\s*(?:am|pm)?\b/i.test(selection)) {
     result = getAllTimeConversions(selection);
   } else {
@@ -34,7 +33,6 @@ async function get_conversions(selection) {
         //console.log('measure_RE', measure_RE);
         //console.log('selection', selection);
         const matches = selection.match(measure_RE);
-        //console.log('matched', matches);
         if (matches != null && matches[0] != null) {
           const quantity = matches[1]
             .toLowerCase()
@@ -43,39 +41,90 @@ async function get_conversions(selection) {
             .replace('−', '-');
           const precision = getPrecision(Number(quantity));
 
-          if (unitObject.unit == 'Dollar') {
-            const promptMessage = 'Multiple matches found. Please choose one:';
-            const options = ['USD', 'CAD', 'AUD'];
-            const userChoice = await customPrompt(promptMessage, options);
-            if (userChoice !== null) {
-              unitObject.unit = options[userChoice];
-              console.log('User choice:', options[userChoice]);
-            } else {
-              console.log('User cancelled the selection.');
-            }
-          } else if (unitObject.unit == 'Yen/Yuan') {
-            const promptMessage = 'Multiple matches found. Please choose one:';
-            const options = ['JPY', 'CNY'];
-            const userChoice = await customPrompt(promptMessage, options);
-            if (userChoice !== null) {
-              unitObject.unit = options[userChoice];
-              console.log('User choice:', options[userChoice]);
-            } else {
-              console.log('User cancelled the selection.');
-            }
-          }
+
+          // const dollar = ['dollars','dollar','USD','$','Can$','C$','CA$','CAD','AU$','A$','AUD'];
+          // const yen_yuan = ['CNY', 'Chinese Yuan', 'Yuan', '¥', 'Yen', 'JPY'];
+          // if (unitObject.type === 'currency' && (yen_yuan.includes(unitObject.unit) || dollar.includes(unitObject.unit))) {
+          //   console.log("qwertyuiopasdfghjk",yen_yuan.includes(unitObject.unit), unitObject.unit)
+          //   options = null;
+          //   if (dollar.includes(unitObject.unit))
+          //     options = ['USD', 'CAD', 'AUD'];
+          //   else if (yen_yuan.includes(unitObject.unit))
+          //     options = ['JPY', 'CNY'];
+
+          //   if (options !== null) {
+          //     const promptMessage = 'Multiple matches found. Please choose one:';
+          //     const userChoice = await customPrompt(promptMessage, options);
+          //     if (userChoice !== null) {
+          //       unitObject.unit = options[userChoice];
+          //       console.log('User choice:', options[userChoice]);
+          //     } else {
+          //       console.log('User cancelled the selection.');
+          //     }
+          //   }
+            // if (dollar.includes(unitObject.unit)) {
+            //   const promptMessage = 'Multiple matches found. Please choose one:';
+            //   const options = ['USD', 'CAD', 'AUD'];
+            //   const userChoice = await customPrompt(promptMessage, options);
+            //   if (userChoice !== null) {
+            //     unitObject.unit = options[userChoice];
+            //     console.log('User choice:', options[userChoice]);
+            //   } else {
+            //     console.log('User cancelled the selection.');
+            //   }
+            // } 
+            // if (yen_yuan.includes(unitObject.unit)) {
+            //   const promptMessage = 'Multiple matches found. Please choose one:';
+            //   const options = ['JPY', 'CNY'];
+            //   const userChoice = await customPrompt(promptMessage, options);
+            //   if (userChoice !== null) {
+            //     unitObject.unit = options[userChoice];
+            //     console.log('User choice:', options[userChoice]);
+            //   } else {
+            //     console.log('User cancelled the selection.');
+            //   }
+            // }
+          // }
 
           const conversion_class = get_conversion_class(
             unitObject.type,
             unitObject.unit
           );
+
           if (unitObject.type == 'currency') {
+            const dollar = ['dollars','dollar','USD','$','Can$','C$','CA$','CAD','AU$','A$','AUD'];
+            const yen_yuan = ['JPY', 'CNY', 'Chinese Yuan', 'Yuan', '¥', 'Yen'];
+
+            console.log("qwertyuiopasdfghjk",yen_yuan.includes(unitObject.unit), unitObject.unit)
+            console.log("qwertyuiopasdfghjk",dollar.includes(unitObject.unit), unitObject.unit)
+            if (yen_yuan.includes(unitObject.unit) || dollar.includes(unitObject.unit)) {
+              console.log("qwertyuiopasdfghjk", yen_yuan.includes(unitObject.unit), unitObject.unit)
+              var options = [];
+              if (dollar.includes(unitObject.unit))
+                options = ['USD', 'CAD', 'AUD'];
+              else if (yen_yuan.includes(unitObject.unit))
+                options = ['CNY', 'JPY'];
+  
+              if (options !== null) {
+                const promptMessage = 'Multiple matches found. Please choose one:';
+                const userChoice = await customPrompt(promptMessage, options);
+                if (userChoice !== null) {
+                  unitObject.unit = options[userChoice];
+                  console.log('User choice:', options[userChoice]);
+                } else {
+                  console.log('User cancelled the selection.');
+                  continue;
+                }
+              }
+            }
+
             const std_conversion =
               await conversion_class.getStandardConversion(quantity);
             result = await conversion_class.getAllConversions(
               Number(std_conversion),
               precision
             );
+            return result;
           } else if (unitObject.type == 'temperature') {
             const std_conversion =
               conversion_class.getStandardConversion(quantity);
